7e1cf9d3235f3617f07942530ea00b624307d440
Kees den Heijer <kees.denheijer@deltares.nl>
2013-11-05 22:19:06 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'numpy',
+    'scipy',
+    ]
+
+setup(name='pyre',
+      version='5.0.2',
+      description='PyRe (Python Reliability) is a python module for structural reliability analysis',
+      long_description=open('README.rst').read(),
+      license=open('LICENSE').read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "License :: General Public License (GPL)",
+        ],
+      author='Juergen Hackl',
+      author_email='hackl.j@gmx.at',
+      url='http://github.com/hackl/pyre',
+      keywords='structural reliability analysis',
+      install_requires = requires,
+      packages=find_packages(),
+      )
