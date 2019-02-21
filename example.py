b301f20bbecd102459ec3c3af81c53d603f0bfd9
Juergen Hackl <hackl@ibi.baug.ethz.ch>
2019-02-21 09:52:03 +0100

    added a function to get the alpha vector

--- example.py
+++ example.py
@@ -9,7 +9,8 @@ import time
 import datetime
 start_time = time.time()
 
-def example_limitstatefunction(X1,X2,X3):
+
+def example_limitstatefunction(X1, X2, X3):
     """
     example limit state function
     """
@@ -19,48 +20,51 @@ def example_limitstatefunction(X1,X2,X3):
 # Define a main() function.
 def main():
 
-  # Define limit state function
-  # - case 1: define directly as lambda function
-  #limit_state = LimitState(lambda X1,X2,X3: 1 - X2*(1000*X3)**(-1) - (X1*(200*X3)**(-1))**2)
-  # - case 2: use predefined function
-  limit_state = LimitState(example_limitstatefunction)
+    # Define limit state function
+    # - case 1: define directly as lambda function
+    #limit_state = LimitState(lambda X1,X2,X3: 1 - X2*(1000*X3)**(-1) - (X1*(200*X3)**(-1))**2)
+    # - case 2: use predefined function
+    limit_state = LimitState(example_limitstatefunction)
 
-  # Set some options (optional)
-  options = AnalysisOptions()
-  # options.printResults(False)
+    # Set some options (optional)
+    options = AnalysisOptions()
+    # options.printResults(False)
 
-  stochastic_model = StochasticModel()
-  # Define random variables
-  stochastic_model.addVariable( Lognormal('X1',500,100) )
-  stochastic_model.addVariable( Normal('X2',2000,400) )
-  stochastic_model.addVariable( Uniform('X3',5,0.5) )
+    stochastic_model = StochasticModel()
+    # Define random variables
+    stochastic_model.addVariable(Lognormal('X1', 500, 100))
+    stochastic_model.addVariable(Normal('X2', 2000, 400))
+    stochastic_model.addVariable(Uniform('X3', 5, 0.5))
 
-  # If the random variables are correlatet, then define a correlation matrix,
-  # else no correlatin matrix is needed
-  stochastic_model.setCorrelation( CorrelationMatrix([[1.0, 0.3, 0.2],
-                                                      [0.3, 1.0, 0.2],
-                                                      [0.2, 0.2, 1.0]]) )
+    # If the random variables are correlatet, then define a correlation matrix,
+    # else no correlatin matrix is needed
+    stochastic_model.setCorrelation(CorrelationMatrix([[1.0, 0.3, 0.2],
+                                                       [0.3, 1.0, 0.2],
+                                                       [0.2, 0.2, 1.0]]))
 
-  # Performe FORM analysis
-  Analysis = Form(analysis_options=options, stochastic_model=stochastic_model, limit_state=limit_state)
+    # Performe FORM analysis
+    Analysis = Form(analysis_options=options,
+                    stochastic_model=stochastic_model, limit_state=limit_state)
 
-  # Performe Distribution analysis
-  Analysis = DistributionAnalysis(analysis_options=options, stochastic_model=stochastic_model, limit_state=limit_state)
-  # Performe Crude Monte Carlo Simulation
-  Analysis = CrudeMonteCarlo(analysis_options=options, stochastic_model=stochastic_model, limit_state=limit_state)
+    # Performe Distribution analysis
+    Analysis = DistributionAnalysis(
+        analysis_options=options, stochastic_model=stochastic_model, limit_state=limit_state)
+    # Performe Crude Monte Carlo Simulation
+    Analysis = CrudeMonteCarlo(
+        analysis_options=options, stochastic_model=stochastic_model, limit_state=limit_state)
 
-  # Performe Importance Sampling
-  Analysis = ImportanceSampling(analysis_options=options, stochastic_model=stochastic_model, limit_state=limit_state)
+    # Performe Importance Sampling
+    Analysis = ImportanceSampling(
+        analysis_options=options, stochastic_model=stochastic_model, limit_state=limit_state)
 
-  # Some single results:
-  beta = Analysis.getBeta()
-  failure = Analysis.getFailure()
+    # Some single results:
+    beta = Analysis.getBeta()
+    failure = Analysis.getFailure()
 
-  print("Beta is {}, corresponding to a failure probability of {}".format(beta,failure))
-  run_time = time.time() - start_time
-  print(str(datetime.timedelta(seconds=run_time)))
+    print("Beta is {}, corresponding to a failure probability of {}".format(beta, failure))
+    run_time = time.time() - start_time
+    print(str(datetime.timedelta(seconds=run_time)))
 
-
-  # This is the standard boilerplate that calls the main() function.
+    # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
-  main()
+    main()
--- pyre/form.py
+++ pyre/form.py
@@ -14,240 +14,254 @@ from .cholesky import *
 from .limitstate import *
 from .stepsize import *
 
+
 class Form(object):
-  """First Order Reliability Method (FORM)
+    """First Order Reliability Method (FORM)
 
-  Let :math:`{\\bf Z}` be a set of uncorrelated and standardized normally
-  distributed random variables :math:`( Z_1 ,\dots, Z_n )` in the normalized
-  z-space, corresponding to any set of random variables :math:`{\\bf X} = (
-  X_1 , \dots , X_n )` in the physical x-space, then the limit state surface
-  in x-space is also mapped on the corresponding limit state surface in
-  z-space.
+    Let :math:`{\\bf Z}` be a set of uncorrelated and standardized normally
+    distributed random variables :math:`( Z_1 ,\dots, Z_n )` in the normalized
+    z-space, corresponding to any set of random variables :math:`{\\bf X} = (
+    X_1 , \dots , X_n )` in the physical x-space, then the limit state surface
+    in x-space is also mapped on the corresponding limit state surface in
+    z-space.
 
-  The reliability index :math:`\\beta` is the minimum distance from the
-  z-origin to the failure surface. This distance :math:`\\beta` can directly
-  be mapped to a probability of failure
+    The reliability index :math:`\\beta` is the minimum distance from the
+    z-origin to the failure surface. This distance :math:`\\beta` can directly
+    be mapped to a probability of failure
 
-  .. math::
-     :label: eq:2_90
+    .. math::
+       :label: eq:2_90
 
-            p_f \\approx p_{f1} = \Phi(-\\beta)
+              p_f \\approx p_{f1} = \Phi(-\\beta)
 
-  this corresponds to a linearization of the failure surface. The
-  linearization point is the design point :math:`{\\bf z}^*`. This procedure
-  is called First Order Reliability Method (FORM) and :math:`\beta` is the
-  First Order Reliability Index. [Madsen2006]_
+    this corresponds to a linearization of the failure surface. The
+    linearization point is the design point :math:`{\\bf z}^*`. This procedure
+    is called First Order Reliability Method (FORM) and :math:`\beta` is the
+    First Order Reliability Index. [Madsen2006]_
 
-  :Attributes:
-    - analysis_option (AnalysisOption): Option for the structural analysis
-    - limit_state (LimitState): Information about the limit state
-    - stochastic_model (StochasticModel): Information about the model
-  """
+    :Attributes:
+      - analysis_option (AnalysisOption): Option for the structural analysis
+      - limit_state (LimitState): Information about the limit state
+      - stochastic_model (StochasticModel): Information about the model
+    """
 
-  def __init__(self,analysis_options=None,limit_state=None,stochastic_model=None):
+    def __init__(self, analysis_options=None, limit_state=None, stochastic_model=None):
 
-    # The stochastic model
-    if stochastic_model is None:
-      self.model = StochasticModel()
-    else:
-      self.model = stochastic_model
+        # The stochastic model
+        if stochastic_model is None:
+            self.model = StochasticModel()
+        else:
+            self.model = stochastic_model
 
-    # Options for the calculation
-    if analysis_options is None:
-      self.options = AnalysisOptions()
-    else:
-      self.options = analysis_options
+        # Options for the calculation
+        if analysis_options is None:
+            self.options = AnalysisOptions()
+        else:
+            self.options = analysis_options
 
-    # The limit state function
-    if limit_state is None:
-      self.limitstate = LimitState()
-    else:
-      self.limitstate = limit_state
+        # The limit state function
+        if limit_state is None:
+            self.limitstate = LimitState()
+        else:
+            self.limitstate = limit_state
 
-    self.i = None
-    self.u = None
-    self.x = None
-    self.J = None
-    self.G = None
-    self.Go = None
-    self.gradient = None
-    self.alpha = None
-    self.gamma = None
-    self.d = None
-    self.step = None
-    self.beta = None
-    self.Pf = None
+        self.i = None
+        self.u = None
+        self.x = None
+        self.J = None
+        self.G = None
+        self.Go = None
+        self.gradient = None
+        self.alpha = None
+        self.gamma = None
+        self.d = None
+        self.step = None
+        self.beta = None
+        self.Pf = None
 
-    # Computation of modified correlation matrix R0
-    computeModifiedCorrelationMatrix(self)
+        # Computation of modified correlation matrix R0
+        computeModifiedCorrelationMatrix(self)
 
-    # Cholesky decomposition
-    computeCholeskyDecomposition(self)
+        # Cholesky decomposition
+        computeCholeskyDecomposition(self)
 
-    # Compute starting point for the algorithm
-    self.computeStartingPoint()
+        # Compute starting point for the algorithm
+        self.computeStartingPoint()
 
-    # Iterations
-    # Set parameters for the iterative loop
-    # Initialize counter
-    i = 1
-    # Convergence is achieved when convergence is set to True
-    convergence = False
+        # Iterations
+        # Set parameters for the iterative loop
+        # Initialize counter
+        i = 1
+        # Convergence is achieved when convergence is set to True
+        convergence = False
 
-    # loope
-    while not convergence:
-      if self.options.printOutput():
-        print('.......................................')
-        print('Now carrying out iteration number:',i)
+        # loope
+        while not convergence:
+            if self.options.printOutput():
+                print('.......................................')
+                print('Now carrying out iteration number:', i)
 
-      # Compute Transformation from u to x space
-      self.computeTransformation()
+            # Compute Transformation from u to x space
+            self.computeTransformation()
 
-      # Compute the Jacobian
-      self.computeJacobian()
+            # Compute the Jacobian
+            self.computeJacobian()
 
-      # Evaluate limit-state function and its gradient
-      self.computeLimitState()
+            # Evaluate limit-state function and its gradient
+            self.computeLimitState()
 
-      # Set scale parameter Go and inform about struct. resp.
-      if i == 1:
-        self.Go = self.G
+            # Set scale parameter Go and inform about struct. resp.
+            if i == 1:
+                self.Go = self.G
+                if self.options.printOutput():
+                    print('Value of limit-state function in the first step:', self.G)
+
+            # Compute alpha vector
+            self.computeAlpha()
+
+            # Compute gamma vector
+            self.computeGamma()
+
+            # Check convergence
+            condition1 = np.absolute(
+                self.G*self.Go**(-1)) < self.options.getE1()
+            condition2 = np.linalg.norm(
+                self.u-self.alpha.dot(self.u).dot(self.alpha)) < self.options.getE2()
+            condition3 = i == self.options.getImax()
+
+            if condition1 and condition2 or condition3:
+                self.i = i
+                convergence = True
+
+            # space for some recording stuff
+
+            # Take a step if convergence is not achieved
+            if not convergence:
+                # Determine search direction
+                self.computeSearchDirection()
+
+                # Determine step size
+                self.computeStepSize()
+
+                # Determine new trial point
+                u_new = self.u + self.step * self.d
+
+                # Prepare for a new round in the loop
+                self.u = u_new[0]  # np.transpose(u_new)
+                i += 1
+
+        # Compute beta value
+        self.computeBeta()
+
+        # Compute failure probability
+        self.computeFailureProbability()
+
+        # Show Results
         if self.options.printOutput():
-          print('Value of limit-state function in the first step:', self.G)
+            self.showResults()
 
-      # Compute alpha vector
-      self.computeAlpha()
+    def computeStartingPoint(self):
+        """Compute starting point for the algorithm"""
+        x = np.array([])
+        marg = self.model.getMarginalDistributions()
+        for i in range(len(marg)):
+            x = np.append(x, marg[i].getStartPoint())
+        self.u = x_to_u(x, self.model)
 
-      # Compute gamma vector
-      self.computeGamma()
+    def computeTransformation(self):
+        """Compute transformation from u to x space"""
+        self.x = np.transpose([u_to_x(self.u, self.model)])
 
-      # Check convergence
-      condition1 = np.absolute(self.G*self.Go**(-1))<self.options.getE1()
-      condition2 = np.linalg.norm(self.u-self.alpha.dot(self.u).dot(self.alpha)) < self.options.getE2()
-      condition3 = i==self.options.getImax()
+    def computeJacobian(self):
+        """Compute the Jacobian"""
+        J_u_x = jacobian(self.u, self.x, self.model)
+        J_x_u = np.linalg.inv(J_u_x)
+        self.J = J_x_u
 
-      if condition1 and condition2 or condition3:
-        self.i = i
-        convergence = True
+    def computeLimitState(self):
+        """Evaluate limit-state function and its gradient"""
+        G, gradient = evaluateLimitState(
+            self.x, self.model, self.options, self.limitstate)
+        self.G = G
+        self.gradient = np.dot(np.transpose(gradient), self.J)
 
-      # space for some recording stuff
+    def computeAlpha(self):
+        """Compute alpha vector"""
+        self.alpha = -self.gradient * np.linalg.norm(self.gradient)**(-1)
 
-      # Take a step if convergence is not achieved
-      if not convergence:
-        # Determine search direction
-        self.computeSearchDirection()
+    def computeGamma(self):
+        """Compute gamma vector"""
+        self.gamma = np.diag(
+            np.diag(np.sqrt(np.dot(self.J, np.transpose(self.J)))))
+        # Importance vector gamma
+        matmult = np.dot(np.dot(self.alpha, self.J), self.gamma)
+        importance_vector_gamma = (matmult*np.linalg.norm(matmult)**(-1))
 
-        # Determine step size
-        self.computeStepSize()
+    def computeSearchDirection(self):
+        """Determine search direction"""
+        self.d = (self.G * np.linalg.norm(self.gradient)**(-1) +
+                  self.alpha.dot(self.u)) * self.alpha - self.u
 
-        # Determine new trial point
-        u_new = self.u + self.step * self.d
+    def computeStepSize(self):
+        """Determine step size"""
+        if self.options.getStepSize() == 0:
+            self.step = getStepSize(
+                self.G, self.gradient, self.u, self.d, self.model, self.options, self.limitstate)
+        else:
+            self.step = self.options.getStepSize()
 
-        # Prepare for a new round in the loop
-        self.u = u_new[0]#np.transpose(u_new)
-        i += 1
+    def computeBeta(self):
+        """Compute beta value"""
+        self.beta = np.dot(self.alpha, self.u)
 
-    # Compute beta value
-    self.computeBeta()
+    def computeFailureProbability(self):
+        """Compute probability of failure"""
+        self.Pf = Normal.cdf(-self.beta, 0, 1)
 
-    # Compute failure probability
-    self.computeFailureProbability()
+    def showResults(self):
+        """Show results"""
+        print('')
+        print('==================================================')
+        print('')
+        print(' RESULTS FROM RUNNING FORM RELIABILITY ANALYSIS')
+        print('')
+        print(' Number of iterations:     ', self.i)
+        print(' Reliability index beta:   ', self.beta[0])
+        print(' Failure probability:      ', self.Pf)
+        print(' Number of calls to the limit-state function:',
+              self.model.getCallFunction())
+        print('')
+        print('==================================================')
+        print('')
 
-    # Show Results
-    if self.options.printOutput():
-      self.showResults()
+    def getBeta(self):
+        """Returns the beta value
 
+        :Returns:
+          - beta (float): Returns the beta value
+        """
+        return self.beta[0]
 
-  def computeStartingPoint(self):
-    """Compute starting point for the algorithm"""
-    x = np.array([])
-    marg = self.model.getMarginalDistributions()
-    for i in range(len(marg)):
-      x = np.append(x,marg[i].getStartPoint())
-    self.u = x_to_u(x,self.model)
+    def getFailure(self):
+        """Returns the probability of failure
 
-  def computeTransformation(self):
-    """Compute transformation from u to x space"""
-    self.x = np.transpose([u_to_x(self.u,self.model)]);
+        :Returns:
+          - Pf (float): Returns the probability of failure
+        """
+        return self.Pf
 
-  def computeJacobian(self):
-    """Compute the Jacobian"""
-    J_u_x = jacobian(self.u,self.x,self.model);
-    J_x_u = np.linalg.inv(J_u_x)
-    self.J = J_x_u
+    def getDesignPoint(self):
+        """Returns the design point
 
-  def computeLimitState(self):
-    """Evaluate limit-state function and its gradient"""
-    G, gradient = evaluateLimitState(self.x,self.model,self.options,self.limitstate)
-    self.G = G
-    self.gradient = np.dot(np.transpose(gradient),self.J)
+        :Returns:
+          - u (float): Returns the design point
+        """
+        return self.u
 
-  def computeAlpha(self):
-    """Compute alpha vector"""
-    self.alpha = -self.gradient * np.linalg.norm(self.gradient)**(-1)
+    def getAlpha(self):
+        """Returns the alpha vector
 
-  def computeGamma(self):
-    """Compute gamma vector"""
-    self.gamma = np.diag(np.diag(np.sqrt(np.dot(self.J,np.transpose(self.J)))))
-    # Importance vector gamma
-    matmult = np.dot(np.dot(self.alpha,self.J),self.gamma)
-    importance_vector_gamma = (matmult*np.linalg.norm(matmult)**(-1))
-
-  def computeSearchDirection(self):
-    """Determine search direction"""
-    self.d = ( self.G * np.linalg.norm(self.gradient)**(-1) + self.alpha.dot(self.u) ) * self.alpha - self.u
-
-  def computeStepSize(self):
-    """Determine step size"""
-    if self.options.getStepSize() == 0:
-      self.step = getStepSize(self.G,self.gradient,self.u,self.d,self.model,self.options,self.limitstate)
-    else:
-      self.step = self.options.getStepSize()
-
-  def computeBeta(self):
-    """Compute beta value"""
-    self.beta = np.dot(self.alpha,self.u)
-
-  def computeFailureProbability(self):
-    """Compute probability of failure"""
-    self.Pf = Normal.cdf(-self.beta,0,1)
-
-  def showResults(self):
-    """Show results"""
-    print('')
-    print('==================================================')
-    print('')
-    print(' RESULTS FROM RUNNING FORM RELIABILITY ANALYSIS')
-    print('')
-    print(' Number of iterations:     ',self.i)
-    print(' Reliability index beta:   ',self.beta[0])
-    print(' Failure probability:      ',self.Pf)
-    print(' Number of calls to the limit-state function:',self.model.getCallFunction())
-    print('')
-    print('==================================================')
-    print('')
-
-
-  def getBeta(self):
-    """Returns the beta value
-
-    :Returns:
-      - beta (float): Returns the beta value
-    """
-    return self.beta[0]
-
-  def getFailure(self):
-    """Returns the probability of failure
-
-    :Returns:
-      - Pf (float): Returns the probability of failure
-    """
-    return self.Pf
-
-  def getDesignPoint(self):
-    """Returns the design point
-
-    :Returns:
-      - u (float): Returns the design point
-    """
-    return self.u
+        :Returns:
+          - alpha (np.array): Returns the alpha vector
+        """
+        return self.alpha[0]
