8343fc094a13a15210612221ccb1c53f2f413c89
ccaprani <31228546+ccaprani@users.noreply.github.com>
2020-11-26 22:48:05 +1100

    Updated Orthonormal transform

--- pyre/sorm.py
+++ pyre/sorm.py
@@ -13,11 +13,11 @@ from .distributions import Normal
 class Sorm(object):
     """
     Second Order Reliability Method (SORM)
-    
+
     Unlike FORM, this method approximates the failure surface in standard
     normal space using a quadratic surface. The basic approximation is given
     by [Breitung1984]_:
-    
+
         .. math::
        :label: eq:2_91
 
@@ -28,7 +28,7 @@ class Sorm(object):
     approximation is asymptotic and accurate for higher values of \\beta.
     Also reported is the Hohenbichler and Rackwitz modification to Brietung's
     formula, which is more accurate for lower values of \\beta.
-    
+
     :Attributes:
       - analysis_option (AnalysisOption): Option for the structural analysis
       - limit_state (LimitState): Information about the limit state
@@ -59,13 +59,13 @@ class Sorm(object):
             self.limitstate = LimitState()
         else:
             self.limitstate = limit_state
-        
+
         # Has FORM already been run? If it exists it has, otherwise run it now
         if form is None:
             self.form = Form(self.options, self.limitstate, self.model)
         else:
             self.form = form
-        
+
         self.betaHL = 0
         self.kappa = 0
         self.pf2_breitung = 0
@@ -73,7 +73,7 @@ class Sorm(object):
         self.pf2_breitung_m = 0
         self.betag_breitung_m = 0
         # Could add Tvedt too
-            
+
     def run(self):
         """
         Run SORM analysis
@@ -87,13 +87,13 @@ class Sorm(object):
         self.pf_breitung(self.betaHL,self.kappa)
         self.pf_breitung_m(self.betaHL,self.kappa)
         self.showResults()
-        
+
     def pf_breitung(self,beta,kappa):
         """
         Calculates the probability of failure and generalized reliability
         index using Breitung's (1984) formula. This formula is good for higher
         values of beta.
-        
+
         Breitung, K. 1984 Asymptotic approximations for multinormal integrals.
         J. Eng. Mech. ASCE 110, 357–367.
         """
@@ -105,13 +105,13 @@ class Sorm(object):
             print("*** SORM Breitung error, excessive curavtures")
             self.pf2_breitung = 0
             self.betag_breitung = 0
-    
+
     def pf_breitung_m(self,beta,kappa):
         """
         Calculates the probability of failure and generalized reliability
         index using Breitung's (1984) formula as modified by Hohenbichler and
         Rackwitz (1988). This formula is better for lower values of beta.
-        
+
         Hohenbichler, M. & Rackwitz, R. 1988 Improvement of second-order
         reliability estimates by importance sampling.
         J. Eng. Mech. ASCE 14, 2195–2199.
@@ -125,7 +125,7 @@ class Sorm(object):
             print("*** SORM Breitung Modified error")
             self.pf2_breitung_m = 0
             self.betag_breitung_m = 0
-        
+
     def showResults(self):
         n_hyphen = 54
         print('')
@@ -140,7 +140,7 @@ class Sorm(object):
             print(f"Curavture {i+1}: {k}")
         print("=" * n_hyphen)
         print('')
-        
+
     def showDetailedOutput(self):
         """Get detailed output to console"""
         names = self.form.model.getNames()
@@ -149,7 +149,7 @@ class Sorm(object):
         alpha = self.form.getAlpha()
         betaHL = self.form.beta[0]
         pfFORM = self.form.Pf
-        
+
         n_hyphen = 54
         print('')
         print("=" * n_hyphen)
@@ -163,11 +163,11 @@ class Sorm(object):
         print("{:15s} \t\t {:2.10f}".format("Beta_G Breitung HR",self.betag_breitung_m))
         print("{:15s} \t\t {:d}".format('Model Evaluations',
                                       self.model.getCallFunction()))
-        
+
         print("-" * n_hyphen)
         for i,k in enumerate(self.kappa):
             print(f"Curvature {i+1}: {k}")
-        
+
         print("-" * n_hyphen)
         print("{:10s} \t {:>9s} \t {:>12s} \t {:>9s}".format("Variable",
                                                             'U_star',
@@ -180,21 +180,21 @@ class Sorm(object):
                                                                    alpha[i]))
         print("=" * n_hyphen)
         print('')
-        
+
     def computeHessian(self, diff_type=None):
         """
         Computes the matrix of second derivatives using forward finite
         difference, using the evaluation of the gradient already done
         for FORM, at the design point
-        
+
         Could use numdifftools as external library instead
-    
+
         """
-        
+
         h = 1/self.options.ffdpara
         nrv = self.form.alpha.shape[1]
         hess_G = np.zeros((nrv,nrv))
-        
+
         if diff_type is None:
             # Differentiation based on the gradients
             x0 = self.form.getDesignPoint(uspace=False)
@@ -215,40 +215,40 @@ class Sorm(object):
             # return the corresponding x-matrix. This would make it easier to
             # add more numerical differentiation schemes.
             u0 = self.form.getDesignPoint()
-            
+
             all_x_plus = np.zeros((nrv,nrv))
             all_x_minus = np.zeros((nrv,nrv))
             all_x_both = np.zeros((nrv,int(nrv*(nrv-1)/2)))
-            
+
             for i in range(nrv):
                 # Plus perturbation and transformation
                 u_plus = np.copy(u0)
                 u_plus[i] += h
                 x_plus = u_to_x(u_plus, self.model)
                 all_x_plus[:,i] = x_plus
-                
+
                 # Minus perturbation and transformation
                 u_minus = np.copy(u0)
                 u_minus[i] -= h
                 x_minus = u_to_x(u_minus,self.model)
                 all_x_minus[:,i] = x_minus
-                
+
                 for j in range(i):
                     # Mixed perturbation and transformation
                     u_both = np.copy(u_plus)
                     u_both[j] += h
                     x_both = u_to_x(u_both,self.model)
                     all_x_both[:,int((i-1)*(i)/2)+j] = x_both
-            
+
             # Assemble all x-space vecs, solve for G, then separate
-            all_x = np.concatenate((all_x_plus, all_x_minus, all_x_both),axis=1)
-            all_G,_ = self.evaluateLSF(all_x,calc_gradient=False)
+            all_x = np.concatenate((all_x_plus, all_x_minus, all_x_both), axis=1)
+            all_G, _ = self.evaluateLSF(all_x, calc_gradient=False)
             all_G = all_G.squeeze()
             all_G_plus = all_G[:nrv]
             all_G_minus = all_G[nrv:2*nrv]
             all_G_both = all_G[2*nrv::]
             G = self.form.G
-            
+
             # Now use finite differences to estimate hessian
             for i in range(nrv):
                 # Second-order central difference
@@ -261,21 +261,21 @@ class Sorm(object):
 
         return hess_G
 
-    def evaluateLSF(self,x, calc_gradient=False, u_space=True):
+    def evaluateLSF(self, x, calc_gradient=False, u_space=True):
         """
         For use in computing the Hessian without altering the FORM object.
         Considers the coord transform so the limit state function is evaluated
         in physical coordinates, but gradient returned in u-space.
-        
+
         This code already in FORM, and a more integrated approach would put
         this in a base class for common use.
 
         """
-        G,grad = 0,0
+        G, grad = 0, 0
         x0 = np.copy(x)  # avoid modifying argument in func calls below
-        
+
         if calc_gradient:
-            G, grad = evaluateLimitState(x0,self.model,self.options,
+            G, grad = evaluateLimitState(x0, self.model, self.options,
                                          self.limitstate)
             grad = np.transpose(grad)
             if u_space:
@@ -286,10 +286,10 @@ class Sorm(object):
         else:
             names = self.model.getNames()
             expression = self.limitstate.getExpression()
-            G, _ = computeLimitStateFunction(x0,names,expression)
-        
+            G, _ = computeLimitStateFunction(x0, names, expression)
+
         return G, grad
-    
+
     def orthonormal_matrix(self):
         """
         Computes the rotation matrix of the standard normal coordinate
@@ -298,58 +298,40 @@ class Sorm(object):
         """
         alpha = self.form.alpha.squeeze()
         nrv = len(alpha)
-        A = np.fliplr(np.eye(nrv))
-        A[:,0] = alpha
+        # Assemble A per theory
+        A = np.eye(nrv)
+        A[-1, :] = alpha
+        # Now rotate 270 degrees anticlockwise as MGS expects the reference
+        # vector in the first column
+        A = np.rot90(A, k=3)
+        # Orthoganalize
         Q = self.gram_schmidt(A)
-        R1 = np.fliplr(Q)
+        # And undo this rotation with a final 90 dgree rotation as order of
+        # the column vector entries is not relevant
+        R1 = np.rot90(Q)
         return R1
-    
-    def gram_schmidt(self,A,gs_type=None):
+
+    def gram_schmidt(self, A):
         """
         Creates an orthonormal matrix using the modified Gram-Schmidt process.
-        What is interesting here is that the QR decomposition doesn't work;
-        while it does return an orthonormal matrix, the signs are different
+        Note that QR decomposition doesn't work for this application; while
+        it does return an orthonormal matrix, the signs are different
         to the modified Gram Schmidt. The signs should be arbitrary, but the
-        resulting rotation matrix does care cabout the signs of the Q.
+        resulting rotation matrix does care cabout the signs of the Q, since
+        it is based on the correct direction of the beta vector [Madsen1986]
         """
-        A = np.asfarray(A)  # ensure is float
-        
-        if gs_type == 'qr':
-            # Using QR decmpostion
-            q, r = np.linalg.qr(A)
-            return q
-        
-        elif gs_type == "gs":
-            # Naive Gram-Schmidt procedure
-            def normalize(v):
-                return v / np.sqrt(v.dot(v))
-            n = len(A)
-            A[:, 0] = normalize(A[:, 0])
-            for i in range(1, n):
-                Ai = A[:, i]
-                for j in range(0, i):
-                    Aj = A[:, j]
-                    t = Ai.dot(Aj)
-                    Ai = Ai - t * Aj
-                A[:, i] = normalize(Ai)
-            return A
-        
-        else:  # gs_type is None:
-            # modified Gram-Schmidt
-            A = np.asfarray(A)
-            # assuming A is a square matrix
-            dim = A.shape[0]
-            Q = np.zeros(A.shape)
-            for j in range(dim):
-                q = A[:,j]
-                for i in range(j):
-                    rij = np.dot(q,Q[:,i])
-                    q = q - rij*Q[:,i]
-                rjj = np.linalg.norm(q, ord=2)
-                if np.isclose(rjj,0.0):
-                    raise ValueError("invalid input matrix")
-                else:
-                    Q[:,j] = q/rjj
-            return Q
-        
-        pass
+
+        A = np.asfarray(A)
+        nvr = A.shape[0]
+        Q = np.zeros(A.shape)
+        for j in range(nvr):
+            q = A[:, j]
+            for i in range(j):
+                rij = np.dot(q, Q[:, i])
+                q = q - rij*Q[:, i]
+            rjj = np.linalg.norm(q, ord=2)
+            if np.isclose(rjj, 0.0):
+                raise ValueError("Singular rotation matrix")
+            else:
+                Q[:, j] = q/rjj
+        return Q
