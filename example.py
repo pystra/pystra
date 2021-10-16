3ce2c462b3b0612d8f5d6c623fe9d9884f3429d1
Colin Caprani <colin.caprani@monash.edu>
2021-10-16 21:18:24 +1100

    Added timing example

--- example.py
+++ example.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python -tt
 # -*- coding: utf-8 -*-
 
-# import pyre library
-from pystra import *
-
-from scipy.stats import norm, lognorm, uniform
+import pystra as pr
 import numpy as np
 
-import time
-import datetime
 
-start_time = time.time()
-
-
-def example_limitstatefunction(g, X1, X2, X3):
+def example_limitstatefunction(r, X1, X2, X3):
     """
     example limit state function
     """
-    return g - X2 * (1000 * X3) ** (-1) - (X1 * (200 * X3) ** (-1)) ** 2
+    return r - X2 * (1000 * X3) ** (-1) - (X1 * (200 * X3) ** (-1)) ** 2
 
 
 # Define a main() function.
@@ -27,29 +19,29 @@ def main():
     # - case 1: define directly as lambda function
     # limit_state = LimitState(lambda X1,X2,X3: 1 - X2*(1000*X3)**(-1) - (X1*(200*X3)**(-1))**2)
     # - case 2: use predefined function
-    limit_state = LimitState(example_limitstatefunction)
+    limit_state = pr.LimitState(example_limitstatefunction)
 
     # Set some options (optional)
-    options = AnalysisOptions()
+    options = pr.AnalysisOptions()
     # options.printResults(False)
 
-    stochastic_model = StochasticModel()
+    stochastic_model = pr.StochasticModel()
     # Define random variables
-    stochastic_model.addVariable(Lognormal("X1", 500, 100))
-    stochastic_model.addVariable(Normal("X2", 2000, 400))
-    stochastic_model.addVariable(Uniform("X3", 5, 0.5))
+    stochastic_model.addVariable(pr.Lognormal("X1", 500, 100))
+    stochastic_model.addVariable(pr.Normal("X2", 2000, 400))
+    stochastic_model.addVariable(pr.Uniform("X3", 5, 0.5))
 
     # Define constants
-    stochastic_model.addVariable(Constant("g", 1))
+    stochastic_model.addVariable(pr.Constant("r", 1))
 
     # If the random variables are correlatet, then define a correlation matrix,
     # else no correlatin matrix is needed
     stochastic_model.setCorrelation(
-        CorrelationMatrix([[1.0, 0.3, 0.2], [0.3, 1.0, 0.2], [0.2, 0.2, 1.0]])
+        pr.CorrelationMatrix([[1.0, 0.3, 0.2], [0.3, 1.0, 0.2], [0.2, 0.2, 1.0]])
     )
 
     # Perform FORM analysis
-    Analysis = Form(
+    Analysis = pr.Form(
         analysis_options=options,
         stochastic_model=stochastic_model,
         limit_state=limit_state,
@@ -58,7 +50,7 @@ def main():
     Analysis.showDetailedOutput()
 
     # Perform SORM analysis, passing FORM result if it exists
-    sorm = Sorm(
+    sorm = pr.Sorm(
         analysis_options=options,
         stochastic_model=stochastic_model,
         limit_state=limit_state,
@@ -69,21 +61,21 @@ def main():
     sorm.showDetailedOutput()
 
     # Perform Distribution analysis
-    Analysis = DistributionAnalysis(
+    Analysis = pr.DistributionAnalysis(
         analysis_options=options,
         stochastic_model=stochastic_model,
         limit_state=limit_state,
     )
 
     # Perform Crude Monte Carlo Simulation
-    Analysis = CrudeMonteCarlo(
+    Analysis = pr.CrudeMonteCarlo(
         analysis_options=options,
         stochastic_model=stochastic_model,
         limit_state=limit_state,
     )
 
     # Perform Importance Sampling
-    Analysis = ImportanceSampling(
+    Analysis = pr.ImportanceSampling(
         analysis_options=options,
         stochastic_model=stochastic_model,
         limit_state=limit_state,
@@ -96,11 +88,9 @@ def main():
     print(
         "Beta is {}, corresponding to a failure probability of {}".format(beta, failure)
     )
-    run_time = time.time() - start_time
-    print(str(datetime.timedelta(seconds=run_time)))
+    
 
-    # This is the standard boilerplate that calls the main() function.
-
-
+# This is the standard boilerplate that calls the main() function.
 if __name__ == "__main__":
     main()
+
---
+++ timing.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python -tt
+# -*- coding: utf-8 -*-
+
+"""
+This file examines the speed up possible when using the built-in distribution
+function implementations, rather than the generic (but more comprehensive) ones
+provided by SciPy. Where performance is critical, implementation of a 
+user-defined distribution object that avoids error checks and user input 
+validation on each call could be beneficial.
+
+Called as: `$python timing.py` results in output like:
+    
+    ```
+    Total time taken (s)
+    Built-in: 1.8919757150579244; Scipy: 5.2964311360847205
+    Average time per call (s):
+    Built-in: 0.018919757150579242; Scipy: 0.052964311360847206
+    Built-in speed-up: 2.80
+    ```
+
+"""
+import pystra as pr
+from scipy.stats import norm, lognorm, uniform
+import numpy as np
+import timeit
+
+
+def lsf(r, X1, X2, X3):
+    g = r - X2 * (1000 * X3) ** (-1) - (X1 * (200 * X3) ** (-1)) ** 2
+    return g
+
+
+def run_builtin():
+    """
+    The basic example using built-in distributions
+    """
+    limit_state = pr.LimitState(lsf)
+
+    stochastic_model = pr.StochasticModel()
+    stochastic_model.addVariable(pr.Lognormal("X1", 500, 100))
+    stochastic_model.addVariable(pr.Normal("X2", 2000, 400))
+    stochastic_model.addVariable(pr.Uniform("X3", 5, 0.5))
+    stochastic_model.addVariable(pr.Constant("r", 1))
+
+    stochastic_model.setCorrelation(
+        pr.CorrelationMatrix([[1.0, 0.3, 0.2], [0.3, 1.0, 0.2], [0.2, 0.2, 1.0]])
+    )
+
+    sorm = pr.Sorm(
+        stochastic_model=stochastic_model,
+        limit_state=limit_state,
+    )
+    sorm.run()
+
+
+def run_scipy():
+    """
+    The basic example using Scipy distributions
+    """
+    limit_state = pr.LimitState(lsf)
+
+    stochastic_model = pr.StochasticModel()
+    # Lognormal
+    zeta = (np.log(1 + (100 / 500) ** 2)) ** 0.5
+    lamb = np.log(500) - 0.5 * zeta ** 2
+    stochastic_model.addVariable(
+        pr.ScipyDist("X1", lognorm(s=zeta, scale=np.exp(lamb)))
+    )
+    # Normal
+    stochastic_model.addVariable(pr.ScipyDist("X2", norm(loc=2000, scale=400)))
+    ## Uniform
+    a_b = (0.5 ** 2 * 12) ** (1 / 2)
+    a = (2 * 5 - a_b) / 2
+    stochastic_model.addVariable(pr.ScipyDist("X3", uniform(loc=a, scale=a_b)))
+    # Constant
+    stochastic_model.addVariable(pr.Constant("r", 1))
+
+    stochastic_model.setCorrelation(
+        pr.CorrelationMatrix([[1.0, 0.3, 0.2], [0.3, 1.0, 0.2], [0.2, 0.2, 1.0]])
+    )
+
+    sorm = pr.Sorm(
+        stochastic_model=stochastic_model,
+        limit_state=limit_state,
+    )
+    sorm.run()
+
+
+number = 100
+time_builtin = timeit.timeit(stmt=run_builtin, number=number)
+time_scipy = timeit.timeit(stmt=run_scipy, number=number)
+
+print("Total time taken (s)")
+print(f"Built-in: {time_builtin}; Scipy: {time_scipy}")
+print("Average time per call (s):")
+print(f"Built-in: {time_builtin/number}; Scipy: {time_scipy/number}")
+print(f"Built-in speed-up: {time_scipy/time_builtin:.2f}")
