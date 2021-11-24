f88fcdabc20ca36e7a70a5d86e6bf9530f918bc5
Mayer Melhem <Mayer.Melhem@monash.edu>
2021-11-24 14:01:00 +1100

    considering correlations

--- examples/system_reliability.py
+++ examples/system_reliability.py
@@ -2,76 +2,258 @@
 # -*- coding: utf-8 -*-
 
 """
-This file examplifies the system reliability module. From Melhem Thesis.
+This file examplifies the system reliability module. 
 
 """
 import pystra as pr
-# from scipy.stats import norm, lognorm, uniform
 import numpy as np
-# import timeit
+import matplotlib.pyplot as plt
 
-# Define and create limit state function (the same for all components) 
-def lsf(r, s):
-    return r - s
+# making some options changes
+options = pr.AnalysisOptions()
+options.print_output = False
 
-ls = pr.LimitState(lsf)
+#%%
+"""
+Series system and parallel system 
+correlated random variables in each limit state function 
+same random variables in limit state
+Problem 3 from Bora Gencturk - FERUM manual on MSR
+"""
 
-# Create components
+# LSF 1
 
-comp1 = pr.Component('A',ls)
-comp2 = pr.Component('B',ls)
-comp3 = pr.Component('C',ls)
 
-comp_list = [comp1,comp2,comp3]
+def lsf_1(x1, x2):
+    return 64 - (x1 + 5) ** 2 - x2 ** 2
 
-# Define and name random variables network as seen in limit state functions
 
-## Example parameters
-R1 = 100        # Mean resistance in A
-R2 = 200        # Mean resistance in B
-R3 = 150        # Mean resistance in C
-S1 = 90         # Mean loading in A
-S2 = 190        # Mean loading in B
-S3 = 140        # Mean loading in C
+ls_1 = pr.LimitState(lsf_1)
+comp_1 = pr.Component("g1", ls_1, options)
 
-# create random variables (using bias and cov trick)
-rv_1 = pr.Normal('r', *R1*1.00*np.array([1, 0.1]))
-rv_4 = pr.Normal('s', *S1*1.00*np.array([1, 5/90]))
-rv_2 = pr.Normal('r', *R2*1.00*np.array([1, 0.1]))
-rv_5 = pr.Normal('s', *S2*1.00*np.array([1, 6/190]))
-rv_3 = pr.Normal('r', *R3*1.00*np.array([1, 0.1]))
-rv_6 = pr.Normal('s', *S3*1.00*np.array([1, 4/140]))
+# LSF 2
 
-# Add random variables to components
-comp1.addVariable(rv_1)
-comp1.addVariable(rv_4)
-comp2.addVariable(rv_2)
-comp2.addVariable(rv_5)
-comp3.addVariable(rv_3)
-comp3.addVariable(rv_6)
 
-# See betas for interests 
-comp_betas = []
+def lsf_2(x1, x2):
+    return 64 - (x1) ** 2 - (x2 + 5) ** 2
+
+
+ls_2 = pr.LimitState(lsf_2)
+comp_2 = pr.Component("g2", ls_2, options)
+
+# LSF 3
+
+
+def lsf_3(x1, x2):
+    return 64 - (x1 + 2) ** 2 - (x2 + 3) ** 2
+
+
+ls_3 = pr.LimitState(lsf_3)
+comp_3 = pr.Component("g3", ls_3, options)
+
+# LSF 4
+
+
+def lsf_4(x1, x2):
+    return 64 - (x1 + 3) ** 2 - (x2 + 2) ** 2
+
+
+ls_4 = pr.LimitState(lsf_4)
+comp_4 = pr.Component("g4", ls_4, options)
+
+
+comp_list = [comp_1, comp_2, comp_3, comp_4]
+
+# define random variables and set component correlations
+
+p = 0.2
+# even if this set to zero, there is still correlation because shared variables
 for c in comp_list:
-    comp_betas.append(c.getProbability()[0])
+    c.addVariable(pr.Normal("x1", 0, 2))
+    c.addVariable(pr.Lognormal("x2", 1, 1))
+
+    c.setCorrelation(pr.CorrelationMatrix([[1.0, p], [p, 1.0]]))
 
 # Now create system(s)
 
 series_sys = pr.SeriesSystem(comp_list)
 parallel_sys = pr.ParallelSystem(comp_list)
-serpar_sys = pr.SeriesSystem([comp1,
-                              pr.ParallelSystem([comp2,comp3])])
 
-# For interests, get event probabilities (should be the same since same components!)
-# default is equivalent planes method
+systems = [series_sys, parallel_sys]
 
-series_sys.getEventProbabilities()
-parallel_sys.getEventProbabilities()
-serpar_sys.getEventProbabilities()
+# Define autocorrelation (order as per random variables instances)
+# descring correlation between the same variable
 
+autocorr = [1, 1]
+# full correlation (ie. the exact same variable), (this is default)
+
+comp_betas = []
+for c in comp_list:
+    comp_betas.append(c.getProbability())
+
+for s in systems:
+    s.setCorrelation(np.array(autocorr))
 
 # calculating system beta and pf
 
-series_reliability = series_sys.getReliability()
-parallel_reliability = parallel_sys.getReliability()
-serpar_reliability = serpar_sys.getReliability()
+for s in systems:
+    print(s.getReliability())
+
+# matches with example!
+# Series: 1.034 x 10-1
+# Parallel: 7.677 x 10-3
+
+#%%
+
+"""
+Series system and parallel system with correlated variables: 
+Thoft-Christensen and Sorenson (1982)
+"Reliability of structural systems with correlated elements"
+"""
+
+# Define and create limit state function (the same for all components)
+def lsf(r, s):
+    return r - s
+
+
+# Here r and s can be the same as the component correlations is being provided
+
+ls = pr.LimitState(lsf)
+
+results = {}
+
+# Create n components
+for N in [2, 5, 10]:
+
+    comp_list = []
+    for n in range(0, N):
+        comp_list.append(pr.Component("B " + str(n), ls, options))
+
+    # Create variables such that component beta is the same
+    component_beta = 3.0
+    sd_R = 10  # Dummy Standard deviation resistance
+    mu_R = 90  # Dummy mean resistance (same for all)
+    S = mu_R - component_beta * sd_R  # dummy loading
+
+    # create random variables and add to component
+    for c in comp_list:
+        c.addVariable(pr.Normal("r", mu_R, sd_R))
+        c.addVariable(pr.Constant("s", S))
+
+    # See betas for interests
+    comp_betas = []
+    for c in comp_list:
+        comp_betas.append(c.getProbability()[0])
+
+    # Now create system(s)
+
+    series_sys = pr.SeriesSystem(comp_list)
+    parallel_sys = pr.ParallelSystem(comp_list)
+
+    systems = [series_sys, parallel_sys]
+
+    # Define autocorrelation (order as per random variables instances)
+    # descring correlation between the same variable
+
+    pf_list = {"series": [], "parallel": []}
+
+    for p in np.linspace(0, 1, 11):
+
+        autocorr = [p]
+
+        # Define equal correlation between components
+
+        for s in systems:
+            s.setCorrelation(np.array(autocorr))
+
+        # calculating system beta and pf
+
+        pf_list["series"].append([p, series_sys.getReliability()[-1]])
+        pf_list["parallel"].append([p, parallel_sys.getReliability()[-1]])
+
+    results[N] = {key: np.array(pf_list[key]) for key in pf_list.keys()}
+
+# plot results
+
+fig, ax = plt.subplots(2, 1, figsize=(5, 6), sharex=True)
+sys_dict = {"series": 0, "parallel": 1}
+
+for n, v_dict in results.items():
+    for sys, vals in v_dict.items():
+        ax[sys_dict[sys]].plot(vals[:, 0], vals[:, 1], label="n = " + str(n))
+
+for k, i in sys_dict.items():
+    plt.xlabel(r"$\rho$")  # x_label
+    ax[i].set_ylabel(r"$P_f$ - " + k + " system")  # y_label
+    ax[i].legend()
+    ax[i].grid()
+
+
+plt.savefig("system_validation_plot.pdf")
+
+# series system matches
+# parallel system does not. (ductile system?)
+
+#%%
+"""
+Series system with correlated random variables of same type: 
+Example 1 from Zhou, Gong & Hong (2017)
+"New Perspective on Application of First-Order Reliability
+Method for Estimating System Reliability"
+"""
+
+# Nominal values
+(Dn, tn, SMTS, Po, ln) = (610, 7.16, 517, 6.0, 50)  # mm and MPa
+d_mu = np.array([0.25, 0.30]) * tn  # for defects 1 and 2
+
+# Define and create limit state function (the same for all components)
+def lsf(D, t, s, p, l, d, x):
+
+    geo_term = l ** 2 / (D * t)
+
+    # case for less than 50 (since geo_term is nominal)
+    M = np.sqrt(1 + 0.6275 * geo_term - 0.003375 * geo_term ** 2)
+
+    pb = x * (1.8 * t * s) / D * ((1 - d / t) / (1 - d / (M * t)))
+
+    # lsf for a pipeline defect
+    g = pb - p
+
+    return g
+
+
+ls = pr.LimitState(lsf)
+
+# make components
+comp_list = []
+for n in range(0, 2):
+    comp_list.append(pr.Component("B " + str(n + 1), ls, options))
+
+# add random variables (no correlation within component)
+for i, c in enumerate(comp_list):
+    c.addVariable(pr.Constant("D", Dn))
+    c.addVariable(pr.Constant("t", tn))
+    c.addVariable(pr.Lognormal("s", 1.09 * SMTS, 0.03 * 1.09 * SMTS))
+    c.addVariable(pr.Gumbel("p", 1.05 * Po, 0.1 * 1.05 * Po))
+    c.addVariable(pr.Constant("l", ln))
+    c.addVariable(pr.Weibull("d", d_mu[i], 0.2 * d_mu[i]))
+    c.addVariable(pr.Lognormal("x", 1.10, 0.172 * 1.10))
+
+# check component betas
+comp_betas = []
+for c in comp_list:
+    comp_betas.append(c.getProbability())
+
+# Now create system(s)
+series_sys = pr.SeriesSystem(comp_list)
+
+# Define autocorrelation (order as per random variables instances)
+# descring correlation between the same random variables
+
+autocorr = [0.3, 0.8, 0.5, 0.5]  # in order of s,p,d,x
+
+series_sys.setCorrelation(np.array(autocorr))
+
+# calculating system beta and pf
+
+print(series_sys.getReliability())
--- pystra/system/system.py
+++ pystra/system/system.py
@@ -14,7 +14,7 @@ class Component():
     """
     A component within a system described by a limit state
     """
-    def __init__(self,name,limit_state):
+    def __init__(self,name,limit_state,analysis_options=None):
         """
         Parameters
         ----------
@@ -22,14 +22,21 @@ class Component():
             description of component
         limit_state : LimitState class object
             Information about the limit state
+        analysis_option: AnalysisOption class object
+            Information about the analysis options. 
+            Defaults when not provided.
         """
         self.name = name
         self.limit_state = limit_state
         self.stochastic_model = StochasticModel()  
         # Also instance a stochastic model
-        self.options = AnalysisOptions()  
-        # For now, using default options
         
+        if analysis_options is None:
+            self.options = AnalysisOptions()  
+            # if not specified, using default for now,
+        else:
+            self.options = analysis_options
+
         self.event_vector = [1,0]
         # used to indicate failure or not
 
@@ -39,6 +46,34 @@ class Component():
         """
         self.stochastic_model.addVariable(obj)
         
+    def setCorrelation(self,obj):
+        """
+        Define correlation for stochastic model of component
+        """
+        self.stochastic_model.setCorrelation(obj)
+
+    def setLimitState(self,limit_state):
+        """
+        Change limit state for component
+
+        Parameters
+        ----------
+        limit_state : LimitState class object
+            Information about the limit state
+        """
+        self.limit_state = limit_state
+        
+    def setOptions(self,analysis_options):
+        """
+        Change analysis options for component
+
+        Parameters
+        ----------
+        analysis_option: AnalysisOption class object
+            Information about the analysis options. 
+        """
+        self.options = analysis_options
+    
     def getProbability(self):
         """
         Obtain the beta and alpha using FORM for component
@@ -46,13 +81,12 @@ class Component():
         analysis = Form(analysis_options=self.options,
                         stochastic_model=self.stochastic_model,
                         limit_state=self.limit_state)
-        # get beta
         beta = float(analysis.beta)
         
         # Get unique variable names for alphas (used for alpha_hat)
         variable_list = list(self.stochastic_model.variables.keys())
-        arg_names = [s + '_' + self.name for s in variable_list]
-        # Make dataframe for alphas
+        arg_names = [s for s in variable_list]
+        # Make dataframe for alphas to concat with others later
         alphas = pd.DataFrame(analysis.alpha,columns=arg_names)
         
         return beta,alphas
@@ -70,24 +104,21 @@ class System():
         self.event_vectors = [] # list of individual event_vectors
         self.event_vector = None #  specific system event vectors
         # used to describe which MECE components events occurs in event
-        self.correlation_matrix = None 
-        # correlation matrix between random variables
-    
-        # TODO: stochasticmodel?
-        
+        self.rvs_autocorrelation_matrix = None 
+        # autocorrelation matrix between random variables
+        self.cmp_correlation_matrix = None 
+        # correlation matrix between components
+         
         self.addComponents(obj_list)
         # populate component list
         
     def addComponents(self,obj_list):
         """Append component objects to system list."""
         
-        # TODO: Check if object is already in list
-        
-        # TODO: can we avoid a loop?
         for obj in obj_list: # loop through the objects
         
             if isinstance(obj,System): # if the object is a system
-                self.components = self.components + obj.components
+                self.components += obj.components
                 # append the components of that system
             else: # if the object is a component
                 self.components.append(obj)
@@ -98,16 +129,38 @@ class System():
             
         # now find the combo array used to find the event vector for system
         self.combo_array = np.array(list(product(*self.event_vectors)))
+     
+    def setCorrelation(self, cm_obj, of_components=False):
+        """
+        Define the correlation in system
+        (either for auto correlation variables or correlation of componets)
+
+        Parameters
+        ----------
+        cm_obj : 1D array or Correlation Matrix object
+            correlation matrix in correct order.
+        of_components : bool, optional
+            whether the correlation of the system components
+            rather than of the autocorrelation of variables. 
+            The default is False. Correlation of components is then calculated.
+        """
         
+        # Set correlation matrix
+        if of_components:
+            self.cmp_correlation_matrix = cm_obj
+        else:      
+            self.rvs_autocorrelation_matrix = np.diagflat(cm_obj)
 
     def eventcorrelations(self,alpha_hat):
         """Convert correlations between random variables to correlations 
-        between component events (limit states) 
-        using alpha_hat (Roscoe, 2015)."""
+        between component events (limit states), if not specified"""
         
         a = alpha_hat
-        pk = self.correlation_matrix
-        return np.dot(np.dot(a,pk),a.T)
+        pk = self.rvs_autocorrelation_matrix
+        self.cmp_correlation_matrix = np.dot(np.dot(a,pk),a.T)
+        # and now replace the diagonal with ones 
+        # (correlation between same limit state)
+        np.fill_diagonal(self.cmp_correlation_matrix, 1)
         
     def getReliability(self,method="epm"):
         """
@@ -116,72 +169,85 @@ class System():
         
         """
         # event vector obtained through init
-        self.getEventProbabilities(method) # get event probabilties using method
+        self.getEventProbabilities(method)
         self.system_probability = np.sum(self.event_probabilities*self.event_vector)
         system_beta = -norm.ppf(self.system_probability)
         return system_beta,self.system_probability
-        
-    
+         
     def getEventProbabilities(self,method="epm"):
         """
         Obtains the probability failure of each MECE event
+        for different methods
         epm = equivalent planes method
         
         """
         
-        if method == "epm":
-            # Step 1: Get component probabilities
-            
-            betas = []  # Pre-allocated beta list of floats
-            alphas = []  # Pre-allocated alpha list of dataframes
-            
-            for component in self.components:
-                # Run individual independent FORM analysis
-                beta,alpha = component.getProbability()
-                 
-                betas.append(beta)  # Append
-                alphas.append(alpha)
-             
-            # Convert to arrays
-            betas = np.array(betas)
-            alpha_hat = np.array(pd.concat(alphas).fillna(0))
+        betas = []  # Pre-allocated beta list of floats
+        alphas = []  # Pre-allocated alpha list of dataframes
         
-            # TODO: Check alpha hat dimensions are correct
+        for component in self.components:
+            # Run individual independent FORM analysis
+            beta,alpha = component.getProbability()
             
-            # Correlation
-            if self.correlation_matrix is None: # set default correlation  
-                n_rvs = np.shape(alpha_hat)[1] 
-                # number of random variables in system
-                self.correlation_matrix = np.eye(n_rvs)
-            
-            corr_events = self.eventcorrelations(alpha_hat)
-    
-            # Enumerating MECE events
-            n = len(betas)
-            sign = [1,-1]  # 1 for fail, -1 for success
-            combs = pd.Series(list(product(sign,repeat=n)))
-            
-            # Function for database apply for probabilities
-            def event_prob(event_comb,b,v):
-                z1 = np.zeros((len(event_comb),len(event_comb)))
-                z2 = np.zeros((len(event_comb),len(event_comb)))
-                z1[:,0] = event_comb
-                z2[0,:] = event_comb
-                # Needed to make matrix multipication possible
-                row = np.array(event_comb)
-                return multivariate_normal.cdf(row*-b,
-                                               cov=np.dot(z1,z2)*v,
-                                               allow_singular=True)
-                # allow_singular provides nearest positive definite
-                
-            # database mapping
-            event_probs = combs.apply(event_prob,b=betas,v=corr_events)
-            # convert to list 
-            self.event_probabilities = list(event_probs)
-            
-            
+            betas.append(beta)  # Append
+            alphas.append(alpha)
+         
+        alpha_hat = pd.concat(alphas).fillna(0)
+        # get the alpha matrix for system
+        
+        # Get the random variable order for correlation matirx 
+        self.random_variables = list(alpha_hat.columns) # unique random variables
+        n_rvs = len(self.random_variables)
+        
+        # Convert to arrays
+        betas = np.array(betas)
+        alpha_hat = np.array(alpha_hat)
+        
+        # Correlation
+        if self.rvs_autocorrelation_matrix is None: # set default correlation  
+            # number of random variables in system
+            self.rvs_autocorrelation_matrix = np.eye(n_rvs)
+        
+        self.eventcorrelations(alpha_hat) # find the component correlations
+        
+        if method == "epm":
+            self.eventprobabilities_epm(betas)
         else:
             print("Only equivalent planes method currently supported")
+            
+    def eventprobabilities_epm(self,betas):
+        """
+        Obtains the probability failure of each MECE event with
+        epm = equivalent planes method
+
+        """
+        
+        # Get component probabilities
+        
+        # Enumerating MECE events
+        n = len(betas)
+        sign = [1,-1]  # 1 for fail, -1 for success
+        combs = pd.Series(list(product(sign,repeat=n)))
+        
+        # Function for database apply for probabilities
+        def event_prob(event_comb,b,v):
+            z1 = np.zeros((len(event_comb),len(event_comb)))
+            z2 = np.zeros((len(event_comb),len(event_comb)))
+            z1[:,0] = event_comb
+            z2[0,:] = event_comb
+            # Needed to make matrix multipication possible
+            row = np.array(event_comb)
+            return multivariate_normal.cdf(row*-b,
+                                           cov=np.dot(z1,z2)*v,
+                                           allow_singular=True)
+            # allow_singular provides nearest positive definite
+            
+        # database mapping
+        event_probs = combs.apply(event_prob,b=betas,
+                                  v=self.cmp_correlation_matrix)
+        # convert to list 
+        self.event_probabilities = list(event_probs)
+
 
     
 class SeriesSystem(System):
@@ -201,7 +267,9 @@ class SeriesSystem(System):
 
     def getEventVector(self):
         """
-        Establish the event vector for series system
+        Establish the event vector for series system as
+        1 - (1 - e1)*(1 - e2)....(1 - en) 
+        where e is the component/system event (and columns of combo_array) 
         """
         one_array = np.ones_like(self.combo_array)
         # create an ones array size of combo_array
@@ -210,9 +278,7 @@ class SeriesSystem(System):
         
         return one_vector - np.prod((one_array - self.combo_array),axis=1)
         # calculate series system event vector
-        # 1 - (1 - e1)*(1 - e2)....(1 - en) 
-        # where e is the component/system event (and columns of combo_array) 
-
+        
 
 class ParallelSystem(System):
     """
@@ -231,9 +297,10 @@ class ParallelSystem(System):
         
     def getEventVector(self):
         """
-        Establish the event vector for parallel system
+        Establish the event vector for parallel system as
+        (e1)*(e2)....(en) 
+        where e is the component/system event (and columns of combo_array) 
         """
         return np.prod(self.combo_array,axis=1)
-        # calculate series system event vector
-        # (e1)*(e2)....(en) 
-        # where e is the component/system event (and columns of combo_array) 
+        # calculate parallel system event vector
+        
